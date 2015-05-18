bd9347dad0b0a4ba18d090526e7d983f4a52fe43
4k3R <iamanjithsasindran@gmail.com>
2015-05-18 14:16:27 +0530

    Updated DetailsActivity with cardslib

--- app/src/main/java/anjithsasindran/httpstatuscodes/DetailsActivity.java
+++ app/src/main/java/anjithsasindran/httpstatuscodes/DetailsActivity.java
@@ -4,6 +4,10 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.view.CardView;
+
 /**
  * Created by Anjith Sasindran on 17-May-15.
  * Activity which opens when ListView is clicked, show the details
@@ -22,5 +26,24 @@ public class DetailsActivity extends ActionBarActivity {
         cursor.moveToFirst();
         actionBar.setTitle(cursor.getString(1));
         actionBar.setSubtitle(cursor.getString(2));
+
+        /*
+         * Uses cardslib (https://github.com/gabrielemariotti/cardslib) library by Gabrielle Mariotti
+         * Card layout for Wikipedia description and link
+         */
+        Card wikiCard = new Card(this);
+        CardHeader wikiCardHeader = new CardHeader(this);
+        wikiCard.addCardHeader(wikiCardHeader);
+
+        CardView wikiCardView = (CardView) findViewById(R.id.wiki_card);
+        wikiCardView.setCard(wikiCard);
+
+        //Card Layout for IETF description
+        Card ietfCard = new Card(this);
+        CardHeader ietfCardHeader = new CardHeader(this);
+        ietfCard.addCardHeader(ietfCardHeader);
+
+        CardView ietfCardView = (CardView) findViewById(R.id.ietf_card);
+        ietfCardView.setCard(ietfCard);
     }
 }
---
+++ app/src/main/java/anjithsasindran/httpstatuscodes/SettingsActivity.java
@@ -0,0 +1,22 @@
+package anjithsasindran.httpstatuscodes;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.view.CardView;
+
+/**
+ * Created by Anjith Sasindran on 18-May-15.
+ * Activity for settings
+ */
+public class SettingsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+
+    }
+}
